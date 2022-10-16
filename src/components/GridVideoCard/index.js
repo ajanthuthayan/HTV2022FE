0b23202ca83319071f2ef0accc951871c83a4caf
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-10-16 04:34:17 -0400

    Update

--- src/components/GridVideoCard/index.js
+++ src/components/GridVideoCard/index.js
@@ -1,58 +1,108 @@
+import { useRouter } from "next/router";
 export default function GridVideoCard({ video, index }) {
+	const router = useRouter();
+
+	const onClickHandler = (e) => {
+		e.preventDefault();
+		return router.push(`/video/${video.id}`);
+	};
 	switch (index) {
 		case 0:
 			return (
-				<div style={{ gridColumnStart: "1", gridColumnEnd: "3" }}>
-					<img src={video.thumbnail} style={{ width: "100%" }} />
+				<div
+					style={{
+						cursor: "pointer",
+						gridColumnStart: "1",
+						gridColumnEnd: "3",
+					}}
+					onClick={onClickHandler}
+				>
+					<img src={video.thumbnail} width="100%" />
 				</div>
 			);
 			break;
 		case 1:
 			return (
-				<img
-					src={video.thumbnail}
-					style={{ gridColumnStart: "3", gridColumnEnd: "4" }}
-				/>
+				<div
+					style={{
+						cursor: "pointer",
+						gridColumnStart: "3",
+						gridColumnEnd: "4",
+					}}
+				>
+					<img src={video.thumbnail} />
+				</div>
 			);
 			break;
 		case 2:
 			return (
-				<img
-					src={video.thumbnail}
-					style={{ gridColumnStart: "1", gridColumnEnd: "2", height: "100%" }}
-				/>
+				<div
+					style={{
+						cursor: "pointer",
+						gridColumnStart: "1",
+						gridColumnEnd: "2",
+						height: "100%",
+					}}
+					onClick={onClickHandler}
+				>
+					<img src={video.thumbnail} />
+				</div>
 			);
 			break;
 		case 3:
 			return (
-				<img
-					src={video.thumbnail}
-					style={{ gridColumnStart: "2", gridColumnEnd: "3" }}
-				/>
+				<div
+					style={{
+						cursor: "pointer",
+						gridColumnStart: "2",
+						gridColumnEnd: "3",
+					}}
+					onClick={onClickHandler}
+				>
+					<img src={video.thumbnail} />
+				</div>
 			);
 			break;
 		case 4:
 			return (
-				<img
-					src={video.thumbnail}
-					style={{ gridColumnStart: "3", gridColumnEnd: "4" }}
-				/>
+				<div
+					style={{
+						cursor: "pointer",
+						gridColumnStart: "3",
+						gridColumnEnd: "4",
+					}}
+					onClick={onClickHandler}
+				>
+					<img src={video.thumbnail} />
+				</div>
 			);
 			break;
 		case 5:
 			return (
-				<img
-					src={video.thumbnail}
-					style={{ gridColumnStart: "1", gridColumnEnd: "4" }}
-				/>
+				<div
+					style={{
+						cursor: "pointer",
+						gridColumnStart: "1",
+						gridColumnEnd: "4",
+					}}
+					onClick={onClickHandler}
+				>
+					<img src={video.thumbnail} />
+				</div>
 			);
 			break;
 		case 6:
 			return (
-				<img
-					src={video.thumbnail}
-					style={{ gridColumnStart: "1", gridColumnEnd: "4" }}
-				/>
+				<div
+					style={{
+						cursor: "pointer",
+						gridColumnStart: "1",
+						gridColumnEnd: "4",
+					}}
+					onClick={onClickHandler}
+				>
+					<img src={video.thumbnail} />
+				</div>
 			);
 			break;
 		default:
